404a4eab9fa0b0ef62b01aca293fd11329083e22
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2018-10-03 20:59:41 -0400

    Add files via upload

---
+++ TellyTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TellyTest {
+
+	public static void main(String[] args) {
+		
+		Scanner in= new Scanner(System.in);
+		
+		System.out.println("Please enter your name:  ");
+		
+		String name = "";
+		
+		in.nextLine();
+		
+		System.out.println("Please enter your phone number in the format xxxxxxxxxx:  ");
+		
+		String number= "xxxxxxxxxx";
+		String sub = number.substring(0,4);
+		String sub2 = number.substring(4,7);
+		String sub3 = number.substring(7,11);
+		
+		System.out.printf("Name:          %20.2f", name);
+		System.out.printf("Phone Number:          %20.2f", "(sub)-"+"(sub2)-"+"(sub3)");
+		
+		
+	}
+}
