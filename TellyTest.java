de8b28942435a776550e536b31587b2b2d0b2cb2
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2018-10-04 12:49:56 -0400

    Update TellyTest.java

--- TellyTest.java
+++ TellyTest.java
@@ -15,9 +15,9 @@ public class TellyTest {
 		System.out.println("Please enter your phone number in the format xxxxxxxxxx:  ");
 		
 		String number= "xxxxxxxxxx";
-		String sub = number.substring(0,4);
-		String sub2 = number.substring(4,7);
-		String sub3 = number.substring(7,11);
+		String sub = number.substring(0,3);
+		String sub2 = number.substring(4,6);
+		String sub3 = number.substring(7,10);
 		
 		System.out.printf("Name:          %20.2f", name);
 		System.out.printf("Phone Number:          %20.2f", "(sub)-"+"(sub2)-"+"(sub3)");
